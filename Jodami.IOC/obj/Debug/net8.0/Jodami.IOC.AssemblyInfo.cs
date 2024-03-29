2c8c05845f60f588a42763af82a9739768c71754
mcastroh <m_castrohinostroza@hotmail.com>
2024-03-28 20:47:28 -0500

    commit

--- Jodami.IOC/obj/Debug/net8.0/Jodami.IOC.AssemblyInfo.cs
+++ Jodami.IOC/obj/Debug/net8.0/Jodami.IOC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Jodami.IOC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea213b37e44ec0bcbcfa2b9e958e39a52b6df927")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+756a3602181f22b4144c9b58cc688e19f510f798")]
 [assembly: System.Reflection.AssemblyProductAttribute("Jodami.IOC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Jodami.IOC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
